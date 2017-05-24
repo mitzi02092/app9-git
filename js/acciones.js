aeb2f1868ef38c2ac8ca48062abfcefa6c6ef671
mitzi02092 <mitzi.fernandez.cbtis16@gmail.com>
2017-05-24 13:00:56 -0500

    2subida
    Novena Aplicacion

--- js/acciones.js
+++ js/acciones.js
@@ -62,7 +62,7 @@ function watchPosition() {
 	$('#aaccuracy').html(position.coords.altitudeAccuracy);
 	$('#headingg').html(position.coords.heading);
 	$('#speed').html(position.coords.speed);
-	$('#timestamp').html(position.coords.timestamp);
+	$('#timestamp').html(position.timestamp);
 	};
 	
 	function onError(error) {
